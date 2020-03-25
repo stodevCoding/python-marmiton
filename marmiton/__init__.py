4fe93442329c6c6dfdc3860bf8ceb44d0a92bec3
Stodev <anthony.custodio@edu.itescia.fr>
2020-03-25 16:49:14 +0100

    search ingredient and get recipe from many page on website marmiton

--- marmiton/__init__.py
+++ marmiton/__init__.py
@@ -26,14 +26,67 @@ class Marmiton(object):
 		"""
 		base_url = "http://www.marmiton.org/recettes/recherche.aspx?"
 		query_url = urllib.parse.urlencode(query_dict)
+		num_of_recipe = 0
+		search_data = []
 
-		url = base_url + query_url
+		for i in range (1, 11):
+
+			url = base_url + query_url + "&start=" + str(num_of_recipe) + "&page=" + str(i)
+			print(url)
+
+			html_content = urllib.request.urlopen(url).read()
+			soup = BeautifulSoup(html_content, 'html.parser')
+
+
+			articles = soup.findAll("div", {"class": "recipe-card"})
+
+
+
+			iterarticles = iter(articles)
+			for article in iterarticles:
+				data = {}
+				if article.find("div", {"class": "recipe-card__sponsored"}):
+					continue
+				try:
+					data["name"] = article.find("h4", {"class": "recipe-card__title"}).get_text().strip(' \t\n\r')
+					data["description"] = article.find("div", {"class": "recipe-card__description"}).get_text().strip(' \t\n\r')
+					data["url"] = article.find("a", {"class": "recipe-card-link"})['href']
+					data["rate"] = article.find("span", {"class": "recipe-card__rating__value"}).text.strip(' \t\n\r')
+					try:
+						data["image"] = article.find('img')['src']
+					except Exception as e1:
+						pass
+				except Exception as e2:
+					pass
+				if data:
+					search_data.append(data)
+					num_of_recipe+=1
+		return search_data
+
+	@staticmethod
+	def searchbyingredients(query_dict):
+		"""
+		Search recipes parsing the returned html data.
+		Options:
+		'aqt': string of keywords separated by a white space  (query search)
+		Optional options :
+		'dt': "entree" | "platprincipal" | "accompagnement" | "amusegueule" | "sauce"  (plate type)
+		'exp': 1 | 2 | 3  (plate expense 1: cheap, 3: expensive)
+		'dif': 1 | 2 | 3 | 4  (recipe difficultie 1: easy, 4: advanced)
+		'veg': 0 | 1  (vegetarien only: 1)
+		'rct': 0 | 1  (without cook: 1)
+		'sort': "markdesc" (rate) | "popularitydesc" (popularity) | "" (empty for relevance)
+		"""
+		base_url = "http://www.marmiton.org/recettes/recherche.aspx?"
+		query_url = urllib.parse.urlencode(query_dict)
+
+		url = base_url + query_url + "&st=1"
 
 		html_content = urllib.request.urlopen(url).read()
 		soup = BeautifulSoup(html_content, 'html.parser')
 
 		search_data = []
-                
+
 		articles = soup.findAll("div", {"class": "recipe-card"})
 
 
@@ -80,7 +133,7 @@ class Marmiton(object):
 		except:
 			name = soup.find("h1", {"class", "main-title"}).get_text().strip(' \t\n\r')
 
-		ingredients = [item.text.replace("\n", "").strip() for item in soup.find_all("li", {"class": "recipe-ingredients__list__item"})]
+		ingredients = [item.text.replace("\n", "").strip() for item in soup.find_all("span", {"class": "ingredient"})]
 
 		try:
 			tags = list(set([item.text.replace("\n", "").strip() for item in soup.find('ul', {"class": "mrtn-tags-list"}).find_all('li', {"class": "mrtn-tag"})]))
@@ -116,7 +169,7 @@ class Marmiton(object):
 		steps = []
 		soup_steps = soup.find_all("li", {"class": "recipe-preparation__list__item"})
 		for soup_step in soup_steps:
-			soup_step.find("h3").decompose()
+			# soup_step.find("h3").decompose()
 			steps.append(Marmiton.__clean_text(soup_step))
 
 		image = soup.find("img", {"id": "af-diapo-desktop-0_img"})['src'] if soup.find("img", {"id": "af-diapo-desktop-0_img"}) else ""
