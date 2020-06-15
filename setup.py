355464d3d72f811f6439dfc3e6226ac80bad4187
Stodev <anthony.custodio@edu.itescia.fr>
2020-06-15 18:34:03 +0200

    update lib python marmiton url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	version='0.3.2',
 	description='Easy-to-use Python API for the marmiton.com cooking website.',
 	packages=['marmiton'],
-	url='https://github.com/remaudcorentin-dev/python-marmiton',
+	url='https://github.com/stodevCoding/python-marmiton.git',
 	author='Corentin Remaud',
 	author_email='remaudcorentin.dev@gmail.com',
 	license='MIT',
